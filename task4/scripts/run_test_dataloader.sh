ea32435ac726d0622065c3b57e8948c1e5eddfc1
Mina Naseh mina.naseh.001@student.uni.lu <mina.naseh98@gmail.com>
2025-01-06 18:46:01 +0100

    packaging

--- task4/scripts/run_test_dataloader.sh
+++ task4/scripts/run_test_dataloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=test_dataloader   # Job name
-#SBATCH --output=task4/logs/test_dataloader.out  # Standard output log
-#SBATCH --error=task4/logs/test_dataloader.err   # Standard error log
+#SBATCH --output=logs/test_dataloader.out  # Standard output log
+#SBATCH --error=logs/test_dataloader.err   # Standard error log
 #SBATCH --time=00:10:00              # Time limit (hh:mm:ss)
 #SBATCH --partition=batch            # Use the 'batch' partitions
 #SBATCH --ntasks=1                   # Run a single task
@@ -12,8 +12,7 @@
 # module load python/3.8
 
 # Activate your Python virtual environment
-source ~/.bashrc
-source .venv/bin/activate
+source ../.venv/bin/activate
 
 # Navigate to the project directory and run the test
-python ./task4/tests/test_dataloader.py
+python tests/test_dataloader.py
