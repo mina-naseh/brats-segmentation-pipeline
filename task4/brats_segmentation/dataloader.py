fixing some error in normalization for visualization and including whole image in the transformers

task4/brats_segmentation/dataloader.py
@@ -1,6 +1,7 @@
 import json
 import os
 from pathlib import Path
+from re import S
 from typing import Literal
 from matplotlib.colors import ListedColormap
 from matplotlib.patches import Patch
@@ -21,6 +22,8 @@ from monai.transforms import (
     DeleteItemsd,
     AsDiscreted,
     CropForegroundd,
+    SpatialPadd,
+    Resized,
 )
 from monai.data import CacheDataset, DataLoader
 from monai.utils import set_determinism
@@ -97,20 +100,27 @@ def get_transforms(roi_size, augment=True):
         #     k_divisible=roi_size,
         #     allow_smaller=False,
         # ),
-        Spacingd(
-            keys=["image", "label"],
-            pixdim=(1.0, 1.0, 1.0),
-            mode=("bilinear", "nearest"),
-        ),
-        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+        # Spacingd(
+        #     keys=["image", "label"],
+        #     pixdim=(1.0, 1.0, 1.0),
+        #     mode=("bilinear", "nearest"),
+        # ),
         # ConvertToMultiChannelBasedOnBratsClassesd(
         #     keys=["label"]
         # ),  # One-hot encode labels
-        RemapLabels(keys=["label"], mapping={4: 1, 2: 1}),
-        AsDiscreted(keys="label", to_onehot=2),  # If using 2 classes
-        # RemapLabels(keys=["label"], mapping={4:3}),
-        # AsDiscreted(keys="label", to_onehot=4), # If using 4 classes
-        RandSpatialCropd(keys=["image", "label"], roi_size=roi_size, random_size=False),
+        # RemapLabels(keys=["label"], mapping={4: 1, 2: 1}),
+        # AsDiscreted(keys="label", to_onehot=2),  # If using 2 classes
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        Resized(
+            keys=["image", "label"],
+            spatial_size=roi_size,
+            mode=("trilinear", "nearest"),
+        ),
+        NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+        # SpatialPadd(keys=["image", "label"], spatial_size=roi_size),
+        # RandSpatialCropd(keys=["image", "label"], roi_size=roi_size, random_size=False),
+        RemapLabels(keys=["label"], mapping={4: 3}),
+        AsDiscreted(keys="label", to_onehot=4),  # If using 4 classes
     ]
 
     if augment:
@@ -132,7 +142,7 @@ def get_dataloader(
     roi_size: tuple,
     batch_size: int = 2,
     num_workers: int = 0,
-    cache_rate: float = 0.5,
+    cache_rate: float = 0.01,
 ):
     """
     Create a PyTorch DataLoader for the BraTS dataset.
@@ -155,7 +165,7 @@ def get_dataloader(
     dataset = CacheDataset(
         data=files,
         transform=transform,
-        cache_rate=0.01,
+        cache_rate=cache_rate,
         num_workers=num_workers,
     )
     return DataLoader(
@@ -185,26 +195,24 @@ def get_dataloaders(split_dir, roi_size, batch_size, num_workers=4):
 
 def make_images(images, segmentation, slice_index, label_color):
     imgs = images[..., slice_index]
-    imgs = (imgs - imgs.min(axis=0)) / (imgs.max(axis=0) - imgs.min(axis=0) + 1e-5)
+    imgs = imgs - imgs.min(axis=(1, 2), keepdims=True)
+    imgs = imgs / (imgs.max(axis=(1, 2), keepdims=True) + 1e-7)
+    # imgs = (imgs * 255).astype(np.uint8)
+    assert (
+        imgs.min() <= 0.0001 and imgs.max() >= 0.999
+    ), f"Images should be normalized, {imgs.min()=} {imgs.max()=}"
     seg = segmentation[..., slice_index]
     for img in imgs:
-
+        img_orig = img.copy()
         img = np.stack([img, img, img], axis=-1)
         for index, color in enumerate(label_color, start=1):
             img[seg == index] = color
+            pass
 
-        yield img
+        yield img, img_orig
 
 
-# Visualization utility using Weights & Biases
-def visualize_samples(loader: DataLoader):
-    """
-    Visualize 2D slices of images and labels using Weights & Biases.
-
-    Args:
-        loader: DataLoader to fetch samples from.
-    """
-    print("------------")
+def visualize_image_and_label(image, segmentation):
     # label_color = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
     # label_text = ["Tumor Core", "Whole Tumor", "Enhancing"]
     # label_color = [[0, 0, 1], [0, 1, 0], [1, 1, 0], [1, 0, 0]]
@@ -214,53 +222,68 @@ def visualize_samples(loader: DataLoader):
     #     "IMPOSSIBLE",
     #     "Enhancing Tumor",
     # ]
-    label_color = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
+    color_const = 0.6
+    label_color = [[0, 0, color_const], [0, color_const, 0], [color_const, 0, 0]]
     label_text = ["Necrotic and Non-Enhancing Tumor", "Edema", "Enhancing Tumor"]
 
-    sample = next(iter(loader))
-
-    # sample[imgage].shape = (batch_size, num_channels, H, W, D)
-    images = sample["image"][0].numpy()  # First item in the batch
-
-    segmentation = sample["label"]
-    print(segmentation.shape)
-    segmentation = torch.argmax(segmentation[0], dim=0).numpy()
     assert (
-        images.shape[1:] == segmentation.shape
-    ), f"Shape mismatch {images.shape=} {segmentation.shape=}"
+        image.ndim == 4
+    ), f"Expected 4D image, (C, H, W, D), got {image.ndim=}, {image.shape=}"
+    assert (
+        segmentation.ndim == 3
+    ), f"Expected 3D segmentation, (H, W, D), got {segmentation.ndim=}, {segmentation.shape=}"
 
-    initial_slice = images.shape[-1] // 2
+    assert (
+        image.shape[-3:] == segmentation.shape
+    ), f"Shape mismatch {image.shape=} {segmentation.shape=}, the last 3 dimensions should be equal"
+
+    initial_slice = image.shape[-1] // 2
 
     # 3D visualization :')
-    if images.shape[0] == 4:
+    if image.shape[0] == 4 and False:
         fig, axes = plt.subplots(2, 2, figsize=(10, 10))
         plt.subplots_adjust(bottom=0.2)
         axes = axes.flatten()
     else:
-        fig, axes = plt.subplots(1, images.shape[0], figsize=(10, 10))
+        fig, all_axes = plt.subplots(2, image.shape[0], figsize=(10, 10))
+        axes = all_axes[0]
+        hists = all_axes[1]
     plt.subplots_adjust(bottom=0.2)
 
     img_displays = []
-    for ax, image, title in zip(
+    for ax, hist, (image_slice, img_orig), title in zip(
         axes,
-        make_images(images, segmentation, initial_slice, label_color),
+        hists,
+        make_images(image, segmentation, initial_slice, label_color),
         ["T1", "T1CE", "T2", "FLAIR"],
     ):
         ax.set_title(title)
         ax.axis("off")
-        img_displays.append(ax.imshow(image))
+        img_displays.append(ax.imshow(image_slice))
+        n, bins, patches = hist.hist(img_orig.ravel(), bins=256)
+        most_freq_idx = np.argmax(n)
+        patches[most_freq_idx].set_height(0)
 
     ax_slider = plt.axes([0.2, 0.05, 0.65, 0.03])  # [left, bottom, width, height]
     slider = Slider(
-        ax_slider, "Slice", 0, images.shape[-1] - 1, valinit=initial_slice, valstep=1
+        ax_slider, "Slice", 0, image.shape[-1] - 1, valinit=initial_slice, valstep=1
     )
 
     def update(_):
         slice_index = int(slider.val)
-        for img_display, img in zip(
-            img_displays, make_images(images, segmentation, slice_index, label_color)
+        for img_display, hist_display, (lbl_img, img) in zip(
+            img_displays,
+            hists,
+            make_images(image, segmentation, slice_index, label_color),
         ):
-            img_display.set_data(img)
+            img_display.set_data(lbl_img)
+            hist_display.clear()
+            n, _, patches = hist_display.hist(img.ravel(), bins=256)
+            most_freq_idx = np.argmax(n)
+            patches[most_freq_idx].set_height(0)
+            hist_display.relim()
+            hist_display.autoscale_view()
+
             fig.suptitle(f"Slice {slice_index}")
         fig.canvas.draw_idle()
 
@@ -275,11 +298,33 @@ def visualize_samples(loader: DataLoader):
     plt.show()
 
 
+def visualize_samples(loader: DataLoader):
+    """
+    Visualize 2D slices of images and labels using Weights & Biases.
+
+    Args:
+        loader: DataLoader to fetch samples from.
+    """
+    print("------------")
+
+    sample = next(iter(loader))
+
+    # sample[imgage].shape = (batch_size, num_channels, H, W, D)
+    images = sample["image"][0].numpy()  # First item in the batch
+
+    segmentation = sample["label"]
+    print(segmentation.shape)
+    segmentation = torch.argmax(segmentation[0], dim=0).numpy()
+    print(segmentation.shape, images.dtype, np.unique(images))
+    visualize_image_and_label(images, segmentation)
+
+
 if __name__ == "__main__":
     # Example usage
     split_dir = "./splits/split3"
-    # roi_size = (128, 128, 128)
-    roi_size = (240, 240, 160)
+    roi_size = (128, 128, 128)
+    # roi_size = (240, 240, 160)
+    # roi_size = (256, 256, 128)
     batch_size = 8
     num_workers = 1
 